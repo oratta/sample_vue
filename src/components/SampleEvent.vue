eab9491e46d7863345d532a27f1350ade6aa2211
oratta <oratta@gmail.com>
2020-03-10 16:26:33 +0900

    add mouth event

--- src/components/SampleEvent.vue
+++ src/components/SampleEvent.vue
@@ -1,20 +1,46 @@
 <template>
-    <form>
-        <label for="name">氏名: </label>
-        <input type="text" id="name"
-               v-on:keyup.esc="clear()"
-               v-on:keyup.ctrl.81="help"
-               v-model="name" />
-    </form>
+    <div class="container">
+        <form>
+            <label for="name">氏名: </label>
+            <input type="text" id="name"
+                   v-on:keyup.esc="clear()"
+                   v-on:keyup.ctrl.81="help"
+                   v-model="name" />
+        </form>
+        <div id="mouth"
+             v-on:click.left="onLeftClick"
+             v-on:click.right.prevent="onRightClick">
+            ボタンの右クリックでコンテキストメニューを表示します
+        </div>
+        <ul class="cxt" v-bind:style="mousePos" v-show="showMenu">
+            <li><a href="">ヘルプ</a></li>
+            <li><a href="">テスト</a></li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'HelloWorld',
         data: ()=>({
-            name: '匿名'
+            name: '匿名',
+            showMenu: false,
+            mousePos: {
+                left: 0,
+                top: 0,
+            },
         }),
         methods: {
+            onLeftClick: function(){
+                this.showMenu = false;
+            },
+            onRightClick: function(e){
+                this.mousePos = {
+                    top: e.pageY + 'px',
+                    left: e.pageX + 'px',
+                }
+                this.showMenu = true;
+            },
             clear: function(){
                 this.name = "";
             },
