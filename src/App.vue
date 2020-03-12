9faa17830f4f6a9d369c1240445c1260a3d95e0f
oratta <oratta@gmail.com>
2020-03-12 17:06:04 +0900

    sample rooter parameter

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/article/188">記事No.188</router-link>|
+      <router-link to="/article/225">記事No.225</router-link>
     </div>
     <router-view/>
   </div>
