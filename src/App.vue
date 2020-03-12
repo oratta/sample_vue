96cae7449ed11ac5f0ec8ac9cffef01027c5195b
oratta <oratta@gmail.com>
2020-03-12 16:21:04 +0900

    install router module

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
 <template>
   <div id="app">
-    <SampleSwitchComponent></SampleSwitchComponent>
-    <SampleEvent></SampleEvent>
-    <SampleSlot>親です</SampleSlot>
-    <SampleSlot></SampleSlot>
-    <div class="sample_component">
-      <SampleComponent v-model="message"></SampleComponent>
-      <p>入力値: {{message}}</p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import SampleEvent from './components/SampleEvent.vue';
-import SampleSlot from './components/SampleSlot.vue';
-import SampleSwitchComponent from './components/SampleSwitchComponent.vue';
-import SampleComponent from './components/SampleComponent.vue';
-
-export default {
-  name: 'App',
-  components: {
-    SampleEvent,
-    SampleSlot,
-    SampleSwitchComponent,
-    SampleComponent,
-  },
-  data: ()=>({
-    current: 0,
-    components: ['Member', 'New', 'Env'],
-    message: 'default'
-  }),
-  computed: {
-
-    currentBanner: function() {
-      return 'Banner' + this.components[this.current];
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -46,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
