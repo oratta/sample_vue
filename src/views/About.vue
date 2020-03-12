ecbf66abb76ec72786a71679b2f776c0fadd8bda
oratta <oratta@gmail.com>
2020-03-12 17:03:06 +0900

    sample routing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button v-on:click="onClick">トップへ</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    onClick: function(){
+      this.$router.push('/');
+    }
+  }
+}
+</script>
