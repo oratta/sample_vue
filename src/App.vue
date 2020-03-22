e4c8ca4a32cde0e8d54f5382813af8db7f8d2c98
oratta <oratta@gmail.com>
2020-03-22 18:46:04 +0900

    router-link attribute sample

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
+      <router-link to="/" exact>Home</router-link> |
+      <router-link to="/about" active-class="current" tag="button" event="mouseover">About</router-link>|
       <router-link to="/article/188">記事No.188</router-link>|
       <router-link to="/article/225">記事No.225</router-link>|
       <router-link to="/article/13">記事No.13</router-link>|
@@ -34,5 +34,11 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.current {
+  color: red;
+  font-weithg: bold;
+  font-size: 2rem;
 }
 </style>
